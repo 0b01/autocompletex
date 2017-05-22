8fe52fa511af2d2c6c23d27c47bca342ba05c42b
ricky han <rickylqhan@gmail.com>
2017-05-22 02:36:22 -0400

    changed elixir version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Autocompletex.Mixfile do
     [
       app: :autocompletex,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       description: description(),
